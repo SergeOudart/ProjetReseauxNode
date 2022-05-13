7c8ea5b1b324869b0774116e624b88fdb8c32fba
GReinert11 <guillaume.reinert11@gmail.com>
2022-05-13 17:07:23 +0200

    Deuxieme modif

--- Client/client.js
+++ Client/client.js
@@ -1,10 +1,12 @@
-const net = require('net');
-import * as WebSocket from 'ws';
+//const net = require('net');
+//const WebSocket = require('ws');
+//import * as WebSocket from 'ws';
 globalThis.socket = new WebSocket("ws://localhost:8999/stomp", "v10.stomp");
 
 
-function sendSubscribe(destination){
+function sendSubscribe(){
     var subscriptionId = Math.floor(Math.random() * 1000);
+    var destination = document.getElementById('listeDestination').values;
     var frame = "SUBSCRIBE\n"
     + `id: ${subscriptionId}\n`
     + `destination: ${destination}\n`
@@ -46,24 +48,24 @@ function unSubscribe(subscriptionId){
 
 
 //Fonction connect si version STOMP 1.2 avec login et mdp
-function connect(login,password){
-   
-    if(!login === "" && mdp === ""){
+function connect(){
+    var login;
+    var password;
+    var login = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if(login === "" && password === ""){
         alert("Saisir un nom d'utilisateur et un mot de passe");
+        
     }else{
-        var login = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        var frame_connect = "CONNECT\n"
-        + "accept-version:1.2\n"
-        +"host://localhost:8999/stomp\n"
-        +"\n"
-        +"^@";
-    
+       
+      
+      
         var frame_stomp = "STOMP\n"
         + "accept-version:1.2\n"
         + "host://localhost:8999/stomp\n"
         +`login:${login}\n`
-        +`password:${paswword}\n`
+        +`password:${password}\n`
         +"heart-beat:\n"
         +"session:\n"
         +"server:\n"
@@ -71,8 +73,32 @@ function connect(login,password){
         +"^@";
 
     }
+    socket.addEventListener('open',function(event){
+        console.log('Connexion avec le serveur établie');
+        //var textearea = document.getElementById('')
+    });
+    socket.addEventListener('message', function (event) {
+        console.log(event.data);
+     /*   if((event).data.includes('CONNECTED')){
+            console.log("connecté");
+        }*/
+    })
+
    
 }
+
+function connectCallBack(){
+
+}
+
+function deconnexion(){
+    const textarea = document.getElementById('chatArea');
+    textarea.value = 'Vous êtes deconnecté du chat';
+    socket.close();
+
+}
+
+
 
 
 
